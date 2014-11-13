92b6ba3c9a5d98be62616beade5f5d881baaba78
sergey <serge.dmitriev@gmail.com>
2014-11-13 23:18:52 +0700

    read and write sip.conf

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var fs = require('fs'),
-    ini = require('ini');
+    ini = require('ini'),
+    i, j;
 
 var path = '/etc/asterisk/',
     files = ['sip.conf'];
 
-var sipconfig = ini.parse(fs.readFileSync(path + files[0], 'utf-8'))
+var Conf = {};
+    Conf[files[0]] = ini.parse(fs.readFileSync(path + files[0], 'utf-8'));
 
-console.log(sipconfig);
+    for (i in Conf[files[0]]){
+        var section = Conf[files[0]][i];
+        for (j in section){
+            section[j] = section[j].trim();
+        }
+    }
+
+console.log(Conf);
+    
+
+fs.writeFileSync('./' + files[0], ini.stringify(Conf[files[0]]));
